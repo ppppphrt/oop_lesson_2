a91a16d6d356cc8f560507ef3678892bd288d2d9
ppppphrt <phavida.r@ku.th>
2023-11-08 11:30:56 +0700

    finished only 2 queries , still have one more queries to do.

--- data_processing.py
+++ data_processing.py
@@ -15,6 +15,25 @@ with open(os.path.join(__location__, 'Countries.csv')) as f:
     for r in rows:
         countries.append(dict(r))
 
+players = []
+with open(os.path.join(__location__, 'Players.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        players.append(dict(r))
+
+teams = []
+with open(os.path.join(__location__, 'Teams.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        teams.append(dict(r))
+
+titanic = []
+with open(os.path.join(__location__, 'Titanic.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        titanic.append(dict(r))
+
+
 class DB:
     def __init__(self):
         self.database = []
@@ -27,13 +46,16 @@ class DB:
             if table.table_name == table_name:
                 return table
         return None
-    
+
+
 import copy
+
+
 class Table:
     def __init__(self, table_name, table):
         self.table_name = table_name
         self.table = table
-    
+
     def join(self, other_table, common_key):
         joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
         for item1 in self.table:
@@ -44,20 +66,20 @@ class Table:
                     dict1.update(dict2)
                     joined_table.table.append(dict1)
         return joined_table
-    
+
     def filter(self, condition):
         filtered_table = Table(self.table_name + '_filtered', [])
         for item1 in self.table:
             if condition(item1):
                 filtered_table.table.append(item1)
         return filtered_table
-    
+
     def aggregate(self, function, aggregation_key):
         temps = []
         for item1 in self.table:
             temps.append(float(item1[aggregation_key]))
         return function(temps)
-    
+
     def select(self, attributes_list):
         temps = []
         for item1 in self.table:
@@ -71,14 +93,24 @@ class Table:
     def __str__(self):
         return self.table_name + ':' + str(self.table)
 
+
 table1 = Table('cities', cities)
 table2 = Table('countries', countries)
+table3 = Table('players', players)
+table4 = Table('teams', teams)
+table5 = Table('titanic', titanic)
 my_DB = DB()
 my_DB.insert(table1)
 my_DB.insert(table2)
+my_DB.insert(table3)
+my_DB.insert(table4)
+my_DB.insert(table5)
 my_table1 = my_DB.search('cities')
+my_table3 = my_DB.search('players')
 
-print("Test filter: only filtering out cities in Italy") 
+print(my_table3)
+print(my_table3.table_name, my_table3.table)
+print("Test filter: only filtering out cities in Italy")
 my_table1_filtered = my_table1.filter(lambda x: x['country'] == 'Italy')
 print(my_table1_filtered)
 print()
@@ -92,11 +124,11 @@ print("Calculting the average temperature without using aggregate for cities in 
 temps = []
 for item in my_table1_filtered.table:
     temps.append(float(item['temperature']))
-print(sum(temps)/len(temps))
+print(sum(temps) / len(temps))
 print()
 
 print("Calculting the average temperature using aggregate for cities in Italy")
-print(my_table1_filtered.aggregate(lambda x: sum(x)/len(x), 'temperature'))
+print(my_table1_filtered.aggregate(lambda x: sum(x) / len(x), 'temperature'))
 print()
 
 print("Test join: finding cities in non-EU countries whose temperatures are below 5.0")
@@ -119,5 +151,29 @@ print("Print the min and max latitude for cities in every country")
 for item in my_table2.table:
     my_table1_filtered = my_table1.filter(lambda x: x['country'] == item['country'])
     if len(my_table1_filtered.table) >= 1:
-        print(item['country'], my_table1_filtered.aggregate(lambda x: min(x), 'latitude'), my_table1_filtered.aggregate(lambda x: max(x), 'latitude'))
+        print(item['country'], my_table1_filtered.aggregate(lambda x: min(x), 'latitude'),
+              my_table1_filtered.aggregate(lambda x: max(x), 'latitude'))
 print()
+
+print()
+print('#1: What player on a team with “ia” in the team name played less than 200 minutes and made more than 100 passes? Select to display the player surname, team, and position')
+print()
+#
+filtered_players = table3.filter(lambda x: 'ia' in x['team'] and int(x['minutes']) < 200 and int(x['passes']) > 100)
+selected_players = filtered_players.select(['surname', 'team', 'position'])
+
+for player in selected_players:
+    print(player)
+
+print()
+print('#2: The average number of games played for teams ranking below 10 versus teams ranking above or equal 10')
+print()
+teams_below_10 = table4.filter(lambda x: int(x['ranking']) < 10)
+teams_above_10 = table4.filter(lambda x: int(x['ranking']) >= 10)
+
+average_games_below_10 = teams_below_10.aggregate(lambda x: sum(x) / len(x), 'games')
+average_games_above_10 = teams_above_10.aggregate(lambda x: sum(x) / len(x), 'games')
+
+print("Average number of games played for teams below 10:", average_games_below_10)
+print("Average number of games played for teams above or equal to 10:", average_games_above_10)
+
