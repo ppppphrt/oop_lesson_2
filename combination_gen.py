1a93e36e3512cc186eb66059b4045ac326c3fafc
ppppphrt <phavida.r@ku.th>
2023-11-13 20:52:08 +0700

    rewrite function gen_comb_list into more uderstandable code

--- combination_gen.py
+++ combination_gen.py
@@ -1,4 +1,4 @@
-
+from itertools import product
 def gen_comb_list(list_set):
     '''
         Parameters:
@@ -13,22 +13,27 @@ def gen_comb_list(list_set):
             gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]) returns [[1, 4, 6], [2, 4, 6], [3, 4, 6], [1, 5, 6], [2, 5, 6], [3, 5, 6], [1, 4, 7], [2, 4, 7], [3, 4, 7], [1, 5, 7], [2, 5, 7], [3, 5, 7], [1, 4, 8], [2, 4, 8], [3, 4, 8], [1, 5, 8], [2, 5, 8], [3, 5, 8]]
     '''
 
-    combinations = [[]]
 
-    # Loop through each list in the list_set
-    for current_list in list_set:
-        new_combinations = []
-        # Combine each element of the current list with the combinations built so far
-        for combination in combinations:
-            for item in current_list:
-                # Append the current item to the existing combination
-                new_combinations.append(combination + [item])
-        # Set combinations to the new combinations for the next iteration
-        combinations = new_combinations
+    for i in list_set:
+        if len(i) > 1:
+            new_list_ii = []
+            for combination in product(*list_set):
+                # print(*original_list_ii)
+                new_list_ii.append(list(combination))
+            return new_list_ii
 
-    return combinations
+        else:
+            new_list = []
+            for i in list_set:
+                # keep_j = []
+                # print(keep_j)
+                for j in i:
+                    keep_j = []
+                    keep_j.append(j)
+                    # print(keep_j)
+                    new_list.append(keep_j)
+            return new_list
 
-
-print(gen_comb_list([[1, 2, 3]]))
-print(gen_comb_list([[1, 2, 3], [4, 5]]))
-print(gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]))
+# print(gen_comb_list([[1, 2, 3]]))
+# print(gen_comb_list([[1, 2, 3], [4, 5]]))
+# print(gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]))
